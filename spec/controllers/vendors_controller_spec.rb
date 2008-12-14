2ae007886adfcaf6fe5abb9b034a786094a89d4a
Ray Baxter <ray.baxter@gmail.com>
2008-12-14 10:38:58 -0800

    Minimal test

--- spec/controllers/vendors_controller_spec.rb
+++ spec/controllers/vendors_controller_spec.rb
@@ -1,8 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe VendorsController do
-  it "should use Active Scaffold" do
-    pending("Needs to be written")
-  end
   
+  describe "active scaffold" do
+    it "should use active scaffold with correctly configured columns for list" do
+      config = controller.active_scaffold_config
+      config.list.columns.map(&:name).should == [:name, :phone_number, :website_url, :email_address]
+    end
+  end
+
+
 end
