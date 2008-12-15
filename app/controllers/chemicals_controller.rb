cce9d63fbd2905ae1f12507d19e165052b8cd0dc
Ray Baxter <ray.baxter@gmail.com>
2008-12-15 15:20:09 -0800

    Move to active scaffold for display

--- app/controllers/chemicals_controller.rb
+++ app/controllers/chemicals_controller.rb
@@ -1,65 +1,9 @@
 class ChemicalsController < ApplicationController
-  def index
-    @chemicals = Chemical.find(:all, :include => :vendor)
-    
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
-  
-  def show
-    @chemical = Chemical.find(params[:id], :include => :vendor)
-  
-    respond_to do |format|
-      format.html # show.html.erb
-    end
-  end
-  
-  def new
-    @chemical = Chemical.new
-    
-    respond_to do |format|
-      format.html # new.html.erb
-    end
-  end
-  
-  def edit
-    @chemical = Chemical.find(params[:id])
-  end
-  
-  def create
-    @chemical = Chemical.new(params[:chemical])
 
-    respond_to do |format|
-      if @chemical.save
-        flash[:notice] = 'Chemical was successfully created.'
-        format.html { redirect_to(@chemical) }
-      else
-        format.html { render :action => "new" }
-      end
-    end
+  active_scaffold :chemical do |config|
+    config.label = "Chemical List"
+    config.columns = [:name, :cas_number, :vendor, :amount, :unit]
+    list.sorting = {:name => 'ASC'}
   end
-  
-  def update
-    @chemical = Chemical.find(params[:id], :include=>:vendor)
-  
-    respond_to do |format|
-      if @chemical.update_attributes(params[:chemical])
-        flash[:notice] = 'Chemical was successfully updated.'
-        format.html { redirect_to(@chemical) }
-      else
-        format.html { render :action => "edit" }
-      end
-    end
-  end
-  
-  def destroy
-    @chemical = Chemical.find(params[:id])
-    @chemical.destroy    
-  
-    respond_to do |format|
-      format.xml  { head :ok }
-      format.html { redirect_to(chemicals_url) }
-    end
-  end
+   
 end
